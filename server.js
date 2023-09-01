57b06747f9d1a53d2fe04481fa1a7fb06fe81bb3
giftthedeveloper <giftjeremiah001@gmail.com>
2023-09-01 12:44:56 +0100

    feat: created route for testimonies

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/testimonies', (req, res) => {
+    res.sendFile(__dirname + '/testimonies.html');
+});
+
+app.get('/events', (req, res) => {
+    res.sendFile(__dirname + '/events.html');
+});
+
+app.get('/feedback', (req, res) => {
+    res.sendFile(__dirname + '/feedback.html');
+});
+
+app.listen(port, () => {
+    console.log(`app is running at port ${port}`)
+});
